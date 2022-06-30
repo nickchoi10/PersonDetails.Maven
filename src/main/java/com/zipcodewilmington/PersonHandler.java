00ccec2069f13895fe80ace5bdb1a5f7e9ee497d
Nick Choi <96934316+nickchoi10@users.noreply.github.com>
2022-06-30 18:39:33 -0400

    finished persondetails lab

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -12,9 +12,10 @@ public class PersonHandler {
 
     public String whileLoop() {
         String result = "";
-        // create a `counter`
-        // while `counter` is less than length of array
-            // begin loop
+        int counter =0; // create a `counter`
+        while(counter <personArray.length){// while `counter` is less than length of array
+            result=result + personArray[counter];
+        counter++;}// begin loop
 
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
@@ -25,13 +26,14 @@ public class PersonHandler {
     }
 
 
-
     public String forLoop() {
         String result = "";
         // identify initial value
         // identify terminal condition
         // identify increment
-
+        for(int i=0; i<personArray.length; i++){
+            result=result + personArray[i];
+        }
         // use the above clauses to declare for-loop signature
             // begin loop
                 // use `counter` to identify the `current Person` in the array
@@ -48,7 +50,10 @@ public class PersonHandler {
         String result = "";
         // identify array's type
         // identify array's variable-name
+        for (Person a:personArray) {
+            result = result + a;
 
+        }
         // use the above discoveries to declare for-each-loop signature
             // begin loop
                 // get `string Representation` of `currentPerson`
